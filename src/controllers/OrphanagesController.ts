bbc6a5c70a381f840599fc30ab7af24dd81a7e74
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-10-13 17:26:49 -0300

    Validations

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
   async index(req: Request, res: Response)  {
@@ -28,10 +29,10 @@ export default {
 
   async create(req: Request, res: Response)  {
     // console.log(req.query);
-  // console.log(req.params);
-  // console.log(req.body);
+    // console.log(req.params);
+    // console.log(req.body);
 
-  // console.log(req.files);
+    // console.log(req.files);
   
 
     const {
@@ -51,7 +52,7 @@ export default {
       return { path: image.filename}
     })
   
-    const orphanage = orphanagesRepository.create({    
+    const data = {
       name,  
       latitude,
       longitude,
@@ -60,7 +61,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      latitude: Yup.string().required(),
+      longitude: Yup.string().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.string().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false,
+    })
+
+    const orphanage = orphanagesRepository.create(data);
   
     await orphanagesRepository.save(orphanage);
   
